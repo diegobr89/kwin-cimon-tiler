479e444238f9d5da3289c9b22c304ad0b289e791
Diego <diegobr89@gmail.com>
2020-06-10 19:58:52 -0300

    Fixed installer and metadata

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 sh make.sh
 rm -rf ~/.local/share/kwin/scripts/cimon-tiler*
-plasmapkg2 --type kwinscript -i cimon-tiler-v*.kwinscript
+plasmapkg2 --type kwinscript -i build/cimon-tiler-v*.kwinscript
 qdbus org.kde.KWin /KWin reconfigure
 mkdir -p ~/.local/share/kservices5
 cp ~/.local/share/kwin/scripts/cimon-tiler/metadata.desktop ~/.local/share/kservices5/kwin-script-cimon-tiler.desktop
